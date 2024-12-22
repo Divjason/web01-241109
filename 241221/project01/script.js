6ecfbfc7886b302749bc1a7caee0aaa9d3dce062
Divjason <sejinpark@tcocs.com>
2024-12-22 17:53:47 +0900

    241222

---
+++ 241221/project01/script.js
@@ -0,0 +1,11 @@
+const circle = document.getElementById("circle");
+const articles = circle.querySelectorAll("article");
+
+articles.forEach((article) => {
+  article.addEventListener("mouseenter", () => {
+    circle.style.animationPlayState = "paused";
+  });
+  article.addEventListener("mouseleave", () => {
+    circle.style.animationPlayState = "running";
+  });
+});

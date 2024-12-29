e47bf1c02beaee7e160a1f3adb4bfcaeb21d1cf4
Divjason <sejinpark@tcocs.com>
2024-12-29 17:50:45 +0900

    241229

--- 241228/responsive/script.js
+++ 241228/responsive/script.js
@@ -1,7 +1,49 @@
+/* scroll event */
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
     document.querySelector("header").classList.add("active");
   } else {
     document.querySelector("header").classList.remove("active");
   }
+});
+
+/* slick slider */
+$(".myslider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  speed: 300,
+  dots: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        speed: 300,
+        dots: true,
+        infinite: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        speed: 300,
+        dots: true,
+        infinite: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 300,
+        dots: true,
+        infinite: false,
+      },
+    },
+  ],
 });
